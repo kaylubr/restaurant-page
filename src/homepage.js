c11943e716db2a11767ea2f2f01a112d10a56891
kaylubr <kylebreyes092005@gmail.com>
2025-09-21 12:36:18 +0800

    Made a homepage module for rendering home page on initial load and by navigating to home

---
+++ src/homepage.js
@@ -0,0 +1,18 @@
+export default function renderHome() {
+  const content = document.querySelector('.content');
+
+  const container = document.createElement('div');
+  container.setAttribute('id', 'home-container'); 
+
+  const header = document.createElement('h1');
+  header.textContent = "Cece's Corner Bistro";
+
+  const caption = document.createElement('p');
+  caption.textContent = 'YOUR NEXT GREAT MEAL STARTS HERE.';
+
+  const button = document.createElement('button');
+  button.textContent = 'BOOK AT TABLE NOW';
+  
+  container.append(header, caption, button);
+  content.append(container);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import "./style.css";
+import Home from './homepage.js';
 
-console.log('Hello, world!');
+const nav = document.querySelector('nav');
+const content = document.querySelector('.content');
+
+nav.addEventListener('click', event => {
+
+  // Resets page every click
+  content.textContent = '';
+
+  const tab = event.target.id;
+  switch(tab) {
+    case 'home':
+      Home();
+      break;
+    case 'menu':
+      break;
+    case 'about':
+      break;
+    case 'contanct':
+      break;
+  }
+})
+
+document.addEventListener('DOMContentLoaded', () => Home());
